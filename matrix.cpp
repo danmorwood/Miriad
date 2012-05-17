d6fdf8c249dbc6b2a89307b5f9e59f4432ef4aba
Daniel Morwood <danmorwood@gmail.com>
2012-05-17 13:46:17 -0600

    Got symbolic Gaussian Elimination working woot

--- matrix.cpp
+++ matrix.cpp
@@ -3,6 +3,22 @@
 
 using namespace std;
 
+Cudd mgr(0,0); 
+
+ADD eq(ADD add, double value) {
+	if(value)
+		return add.Agreement(mgr.constant(value)).Cmpl().Cmpl();
+	else
+		return add.Cmpl();
+}
+
+ADD neq(ADD add, double value) {
+	if(value)
+		return add.Agreement(mgr.constant(value)).Cmpl();
+	else
+		return add.Cmpl().Cmpl();
+}
+
 SparceMatrix::SparceMatrix(unsigned int n, unsigned int m) {
 	sizeN = n;
 	mData.resize(n);
@@ -41,6 +57,32 @@ void SparceMatrix::swapRow(unsigned int r1, unsigned int r2) {
 		return;
 	}
 	mData[r1].swap(mData[r2]);
+}
+
+void SparceMatrix::swapEntryInColumn(unsigned int i, unsigned int k, ADD kPrime) {
+	valueType tmp = get(k, i);
+	valueType A_ki = BACKGROUND;
+	for(int j = k; j < sizeM; j++) {
+		A_ki += get(j, i) * eq(kPrime, j);
+	}
+	set(k,i, A_ki);
+	for(int j = k+1; j < sizeM; j++) {
+		ADD Different = neq(kPrime, j);
+		set(j, i, get(j,i) * Different + tmp * Different.Cmpl());
+	}
+}
+
+void swapInMatrix(ADDvector& vec, int k, ADD p) {
+	valueType tmp = vec[k];
+	valueType newAdd = BACKGROUND;
+	for(int j = k; j < vec.count(); j++) {
+		newAdd += vec[j] * eq(p, j);
+	}
+	vec[k] = newAdd;
+	for(int j = k+1; j < vec.count(); j++) {
+		ADD Different = neq(p, j);
+		vec[j] = Different * vec[j] + tmp * Different.Cmpl();
+	}
 }
 
 void SparceMatrix::swapColumn(unsigned int c1, unsigned int c2) {
@@ -90,86 +132,103 @@ void SparceMatrix::Subtract(unsigned int r1, unsigned int r2) {
 	}
 }
 
-void SparceMatrix::Multipy(unsigned int row, valueType value) {
+void SparceMatrix::SubtractMultiple(unsigned int r1, valueType factor, unsigned int r2) {
+	map<unsigned int, valueType>::iterator r1it = mData[r1].begin();
+	map<unsigned int, valueType>::iterator r2it = mData[r2].begin();
+	while (r1it != mData[r1].end() && r2it != mData[r2].end()) {
+		if(r1it->first < r2it->first) {
+			r1it++;
+		} else if( r2it->first < r1it->first) {
+			mData[r1][r2it->first] = -r2it->second*factor;;
+			r2it++;
+		} else {
+			mData[r1][r1it->first] = r1it->second - r2it->second*factor;
+			r1it++;
+			r2it++;
+		}
+	}
+	while(r2it != mData[r2].end()) {
+		mData[r1][r2it->first] = -r2it->second*factor;
+		r2it++;
+	}
+}
+
+void SparceMatrix::Multiply(unsigned int row, valueType value) {
 	for(map<unsigned int, valueType>::iterator it = mData[row].begin();
 			it != mData[row].end(); it++) {
 		it->second *= value;
 	}
 }
 
-void SparceMatrix::rref() {
-	if(sizeN != sizeM - 1) {
-		printf("rref(): Im not sure what to do with that...\n");
-		return;
-	}
-	for(int i = 0; i < sizeN; i++) {
-		for(int j = i+1; j < sizeN; j++) {
-			double factor = mData[i][i] / mData[j][i];
-			Multipy(j, factor);
-			Subtract(j, i);
+ADDvector SparceMatrix::LUPDecomposition() {
+	for(int r = 0; r < sizeM; r++) {
+		for(int c = 0; c < sizeN; c++) {
+			fprintf(stderr, "%2.0f\t", Cudd_V(get(r, c).getNode()));
 		}
-		Multipy(i, 1/mData[i][i]);
+		fprintf(stderr, "\n");
 	}
-	for(int i = sizeN -1; i >= 0; i--) {
-		for(int j = i-1; j >= 0; j--) {
-			double factor = mData[j][i];
-			Multipy(i, factor);
-			Subtract(j, i);
-			Multipy(i, 1/factor);
-		}
-	}
-}
-
-void SparceMatrix::print() {
-	for(int i = 0; i < sizeN; i++) {
-		for(int j = 0; j < sizeM; j++) {
-			printf("%2.1f ", get(i, j));
-		}
-		printf("\n");
-	}
-}
-
-SparceMatrix SparceMatrix::LUDecomposition() {
-	SparceMatrix ret(sizeN, sizeM);
-	for(int k = 0; k < sizeN; k++) {
-		ret.set(k,k,get(k,k));
-		for(int i = k+1; i < sizeN; i++) {
-			ret.set(i, k, get(i, k)/get(k, k));
-			ret.set(k, i, get(k, i));
-		}
-		for(int i = k+1; i < sizeN; i++) {
-			for(int j = k+1; j < sizeN; j++) {
-				set(i,j,get(i,j)-ret.get(i,k)*ret.get(k,j));
-			}
-		}
-	}
-}
-
-SparceMatrix SparceMatrix::LUPDecomposition() {
-	vector<int> pi(sizeN);
-	for(int i = 0; i < sizeN; i++) {
-		pi[i] = i;
+	ADDvector pi(sizeM, &mgr);
+	for(int i = 0; i < sizeM; i++) {
+		pi[i] = mgr.constant(i);
 	}
 	for(int k = 0; k < sizeN; k++) {
-		int kPrime = k;
+		pi.DumpDot();
 		valueType p = BACKGROUND;
+		ADD kPrime = mgr.constant(k);
 		for(int i = k; i < sizeN; i++) {
-			if(abs(get(i,k)) > p) {
-				p = abd(get(i,k));
-				kPrime = i;
-			}
+			valueType A_ik = get(i, k);
+			valueType abs = A_ik.Maximum(-A_ik);
+			p = p.Maximum(abs); 
+			kPrime = kPrime.Maximum(p.Agreement(abs).Cmpl().Cmpl()*mgr.constant(i));
 		}
-		if(p == 0) {
-			printf("Error Singular Matrix?!?\n");
+		for(int i = 0; i < sizeN; i++) {
+			swapEntryInColumn(i, k, kPrime);
 		}
-		swap(pi[k], pi[kPrime]);
-		swapRow(k, kPrime);
-		for(i = k+1; i < sizeN; i++) {
-			set(i, k, get(i,k)/get(k,k));
-			for(j = k+1; k < n; k++) {
-				set(i,j,get(i,j)-get(i,k)*get(k,j));
+		swapInMatrix(pi, k, kPrime);
+		valueType divisor = get(k, k);
+		for(int i = k+1; i < sizeN; i++) {
+			valueType A_ik = get(i, k).Divide(divisor);
+			fprintf(stderr, "A[%d,%d] = %f\n", i,k, Cudd_V(A_ik.getNode()));
+			for( int j = k+1; j < sizeN; j++) {
+				fprintf(stderr, "A[%d,%d] = %f\n", i,j, Cudd_V(get(i,j).getNode()));
+				fprintf(stderr, "A[%d,%d] = %f\n", k,j, Cudd_V(get(k,j).getNode()));
+				set(i, j, get(i, j) - A_ik * get(k, j));
+				fprintf(stderr, "A[%d,%d] = %f\n", i,j, Cudd_V(get(i,j).getNode()));
 			}
+			set(i,k, A_ik);
 		}
 	}
 	return pi;
 }
+
+ADDvector SparceMatrix::Solve(ADDvector pi, ADDvector b) {
+	ADDvector y(sizeM,&mgr);
+	ADDvector x(sizeM,&mgr);
+	for(int i = 0; i < sizeM; i++) {
+		y[i] = x[i] = BACKGROUND;
+	}
+	for(int i = 0; i < sizeN; i++) {
+		ADD b_pi = BACKGROUND;
+		for(int j = 0; j < sizeM; j++) {
+			b_pi += b[j] * eq(pi[i],j);
+		}
+		fprintf(stderr, "b_pi[%d] = %f\n", i, Cudd_V(b_pi.getNode()));
+		ADD sum = BACKGROUND;
+		for(int j = 0; j <= i-1; j++) {
+			sum += get(i,j)*y[j];
+		}
+		fprintf(stderr, "sum[%d] = %f\n", i, Cudd_V(sum.getNode()));
+		y[i] = b_pi - sum;
+		fprintf(stderr, "y[%d] = %f\n", i, Cudd_V(y[i].getNode()));
+	}
+	for(int i = sizeN-1; i >= 0; i--) {
+		ADD sum = BACKGROUND;
+		for(int j = i+1; j < sizeN; j++) {
+			sum += get(i,j) * x[j];
+		}
+		x[i] = y[i] - sum;
+		x[i] = x[i].Divide(get(i,i));
+	}
+	return x;
+}
+
--- matrix.h
+++ matrix.h
@@ -1,8 +1,10 @@
+#include "cuddObj.hh"
 #include <map>
 #include <vector>
 
-typedef double valueType;
-#define BACKGROUND 0
+extern Cudd mgr;
+typedef ADD valueType;
+#define BACKGROUND mgr.addZero()
 
 class SparceMatrix {
 	public:
@@ -11,12 +13,17 @@ class SparceMatrix {
 		valueType get(unsigned int, unsigned int);
 		void swapRow(unsigned int r1, unsigned int r2);
 		void swapColumn(unsigned int c1, unsigned int c2);
+		void swapEntryInColumn(unsigned int col, unsigned int r1, ADD r2);
 
 		//Subtract r2 from r1 and store result in r1
 		void Subtract(unsigned int r1, unsigned int r2);
-		void Multipy(unsigned int row, valueType value);
+		//Subtract factor*r2 from r1 and store result in r1.
+		void SubtractMultiple(unsigned int r1, valueType factor, unsigned int r2);
+		void Multiply(unsigned int row, valueType value);
 		void rref();
 		void print();
+		ADDvector LUPDecomposition();
+		ADDvector Solve(ADDvector pi, ADDvector b);
 	private:
 		std::vector< std::map<unsigned int, valueType> > mData;
 		int sizeN;
